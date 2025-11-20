81654aade4771b9b9f313883b27ca4588b766905
pers345 <41289817+pers345@users.noreply.github.com>
2025-11-21 01:09:14 +0800

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/carbon-emission-calculator/',
+  base: '/carbon-calculator/',
   plugins: [react()],
 });
